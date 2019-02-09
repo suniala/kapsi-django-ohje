3067208b771cd5ad19a947419a0a6868b136a1e4
Mikko Suniala <mikko.suniala@iki.fi>
2019-02-09 14:57:27 +0200

    Luo tyhjä django-projekti

---
+++ kirja/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kirja project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kirja.settings")
+
+application = get_wsgi_application()
